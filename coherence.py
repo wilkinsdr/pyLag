31a0b57b2cb7d7b1c5d6530b65fcf0b0f4c54225
Dan Wilkins <dan.wilkins@stanford.edu>
2018-02-21 18:12:53 -0800

    Added CoherenceSpectrum

--- coherence.py
+++ coherence.py
@@ -185,3 +185,240 @@ class Coherence(object):
 
     def _getplotaxes(self):
         return 'Frequency / Hz', 'log', 'Coherence', 'linear'
+    
+    
+class CoherenceSpectrum(object):
+    """
+    pylag.CoherenceSpectrum
+
+    Class for computing the coherence as a function of energy from a set of light curves,
+    one in each energy band (or a set of light curve segments in each energy band),
+    relative to a reference band that is the summed time series over all energy
+    bands. The lag at each energy is averaged over some	frequency range. For each
+    energy band, the present energy range is subtracted from the reference band
+    to avoid correlated noise.
+
+    The resulting coherence spectrum is stored in the member variables.
+
+    This class automates calculation of the spectrum from the Coherence classes.
+
+    Member Variables
+    ----------------
+    en       : ndarray
+               numpy array containing the central energy of each band
+    en_error : ndarray
+               numpy array containing the error bar of each energy band (the
+               central energy minus the minimum)
+    coh      : ndarray
+               numpy array containing the coherence in each band across the
+               specified frequency range
+    error    : ndarray
+               for future use
+
+    Constructor: pylag.CoherenceSpectrum(lclist, fmin, fmax, enmin, enmax, lcfiles, interp_gaps=False, refband=None)
+
+    Constructor Arguments
+    ---------------------
+    fmin        : float
+                  Lower bound of frequency range
+    fmin          : float
+                  Upper bound of frequency range
+    lclist      : list of LightCurve objects or list of lists of LightCurve objects
+                  optional (default=None)
+                  This is either a 1-dimensional list containing the pylag
+                  LightCurve objects for the light curve in each of the energy bands,
+                  i.e. [en1_lc, en2_lc, ...]
+                  or a 2-dimensional list (i.e. list of lists) if multiple observation
+                  segments are to be stacked. In this case, the outer index
+                  corresponds to the energy band. For each energy band, there is a
+                  list of LightCurve objects that represent the light curves in that
+                  energy band from each observation segment.
+                  i.e. [[en1_obs1, en1_obs2, ...], [en2_obs1, en2_obs2, ...], ...]
+    enmin       : ndarray or list, optional (default=None)
+                  numpy array or list containing the lower energy bound of each band
+                  (each entry corresponds to one light curve, in order)
+    enmax       : ndarray or list, optional (default=None)
+                  numpy array or list containing the upper energy bound of each band
+                  (each entry corresponds to one light curve, in order)
+    lcfiles     : string, optional (default='')
+                  If not empty, the filesystem will be searched using this glob to
+                  automatically build the list of light curves and energies
+    interp_gaps : boolean (default=False)
+                  Interpolate over gaps in the light curves
+    refband     : list of floats
+                  If specified, the reference band will be restricted to this
+                  energy range [min, max]. If not specified, the full band will
+                  be used for the reference
+    """
+
+    def __init__(self, fmin, fmax, lclist=None, lcfiles='', interp_gaps=False, refband=None,
+                 bias=True):
+        self.en = np.array([])
+        self.en_error = np.array([])
+        self.error = np.array([])
+        self.coh = np.array([])
+
+        if lcfiles != '':
+            lclist = EnergyLCList(lcfiles, interp_gaps=interp_gaps)
+
+        self.en = np.array(lclist.en)
+        self.en_error = np.array(lclist.en_error)
+
+        if isinstance(lclist[0], LightCurve):
+            print("Constructing coherence spectrum in %d energy bins" % len(lclist))
+            self.coh = self.calculate(lclist.lclist, fmin, fmax, refband, self.en, bias)
+        elif isinstance(lclist[0], list) and isinstance(lclist[0][0], LightCurve):
+            print("Constructing coherence spectrum from %d light curves in each of %d energy bins" % (
+                len(lclist[0]), len(lclist)))
+            self.coh = self.calculate_stacked(lclist.lclist, fmin, fmax, refband, self.en, bias)
+
+    def calculate(self, lclist, fmin, fmax, refband=None, energies=None, bias=True):
+        """
+        coh = pylag.CoherenceSpectrum.calculate(lclist, fmin, fmax, refband=None, energies=None)
+
+        calculate the coherence as a function of energy from a list of light curves,
+        one in each energy band, averaged over some frequency range.
+
+        The coherence is calculated with respect to a reference light curve that is
+        computed as the sum of all energy bands, but subtracting the energy band
+        of interest for each point, so to avoid correlated noise
+        between the subject and reference light curves.
+
+        Arguments
+        ---------
+        lclist   : list of LightCurve objects
+                   1-dimensional list containing the pylag
+                   LightCurve objects for the light curve in each of the energy
+                   bands, i.e. [en1_lc, en2_lc, ...]
+        fmin     : float
+                   Lower bound of frequency range
+        fmax     : float
+                   Upper bound of frequency range
+        refband  : list of floats
+                 : If specified, the reference band will be restricted to this
+                   energy range [min, max]. If not specified, the full band will
+                   be used for the reference
+        energies : ndarray (default=None)
+                 : If a specific range of energies is to be used for the reference
+                   band rather than the full band, this is the list of central
+                   energies of the bands represented by each light curve
+        bias     : boolean, optional (default=True)
+                   If true, the bias due to Poisson noise will be subtracted when
+                   calculating coherence
+
+        Returns
+        -------
+        coh   : ndarray
+                numpy array containing the coherence of each energy band with respect
+                to the reference band
+        """
+        reflc = LightCurve(t=lclist[0].time)
+        for energy_num, lc in enumerate(lclist):
+            if refband is not None:
+                if energies[energy_num] < refband[0] or energies[energy_num] > refband[1]:
+                    continue
+            reflc = reflc + lc
+
+        error = []
+        coh = []
+        for lc in lclist:
+            thisref = reflc - lc
+            # if we're only using a specific reference band, we did not need to
+            # subtract the current band if it's outside the range
+            if refband is not None:
+                if energies[energy_num] < refband[0] or energies[energy_num] > refband[1]:
+                    thisref = reflc
+            coh.append(coherence_obj.coh)
+
+        return np.array(coh)
+
+    def calculate_stacked(self, lclist, fmin, fmax, refband=None, energies=None, bias=True):
+        """
+        coh = pylag.CoherenceSpectrum.CalculateStacked(lclist, fmin, fmax, refband=None, energies=None)
+
+        calculate the coherence as a function of energy from a list of light curves,
+        averaged over some frequency range. The coherence spectrum is calculated from
+        the cross spectrum and stacked over multiple light curve
+        segments in each energy band.
+        The coherence is calculated with respect to a reference light curve that is
+        computed as the sum of all energy bands, but subtracting the energy band
+        of interest for each point, so to avoid correlated noise
+        between the subject and reference light curves.
+
+        Arguments
+        ---------
+        lclist   : list of lists of LightCurve objects
+                   This is a 2-dimensional list (i.e. list of lists). The outer index
+                   corresponds to the energy band. For each energy band, there is a
+                   list of LightCurve objects that represent the light curves in that
+                   energy band from each observation segment.
+                   i.e. [[en1_obs1, en1_obs2, ...], [en2_obs1, en2_obs2, ...], ...]
+        fmin     : float
+                   Lower bound of frequency range
+        fmax     : float
+                   Upper bound of frequency range
+        refband  : list of floats
+                 : If specified, the reference band will be restricted to this
+                   energy range [min, max]. If not specified, the full band will
+                   be used for the reference
+        energies : ndarray (default=None)
+                 : If a specific range of energies is to be used for the reference
+                   band rather than the full band, this is the list of central
+                   energies of the bands represented by each light curve
+        bias     : boolean, optional (default=True)
+                   If true, the bias due to Poisson noise will be subtracted when
+                   calculating coherence
+
+        Returns
+        -------
+        coh   : ndarray
+                numpy array containing the coherence of each energy band with respect
+                to the reference band
+        error : ndarray
+                numpy array containing the error in each lag measurement
+        """
+        reflc = []
+        # initialise a reference light curve for each of the observations/light
+        # curve segments
+        for lc in lclist[0]:
+            reflc.append(LightCurve(t=lc.time))
+        # sum all of the energies (outer index) together to produce a reference
+        # light curve for each segment (inner index)
+        for energy_num, energy_lcs in enumerate(lclist):
+            # if a reference band is specifed, skip any energies that do not fall
+            # in that range
+            if refband is not None:
+                if energies[energy_num] < refband[0] or energies[energy_num] > refband[1]:
+                    continue
+            for segment_num, segment_lc in enumerate(energy_lcs):
+                reflc[segment_num] = reflc[segment_num] + segment_lc
+
+        error = []
+        coh = []
+        for energy_num, energy_lclist in enumerate(lclist):
+            # subtract this energy band from the reference light curve for each
+            # light curve segment to be stacked (subtracting the current band
+            # means we don't have correlated noise between the subject and
+            # reference bands)
+            ref_lclist = []
+            for segment_num, segment_lc in enumerate(energy_lclist):
+                # if a reference band is specifed and this energy falls outside that
+                # band, no need to subtract the current band
+                if refband is not None:
+                    if energies[energy_num] < refband[0] or energies[energy_num] > refband[1]:
+                        ref_lclist.append(reflc[segment_num])
+                        continue
+                ref_lclist.append(reflc[segment_num] - segment_lc)
+            # now get the lag and error from the stacked cross spectrum and
+            # coherence between the sets of lightcurves for this energy band and
+            # for the reference
+            coherence_obj = Coherence(energy_lclist, ref_lclist, fmin=fmin, fmax=fmax, bias=bias)
+            coh.append(coherence_obj.coh)
+
+        return np.array(coh)
+
+    def _getplotdata(self):
+        return (self.en, self.en_error), self.coh
+
+    def _getplotaxes(self):
+        return 'Energy / keV', 'log', 'Coherence', 'linear'
