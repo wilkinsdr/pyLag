7eb3cf8be27c959661f5fd6ad048e5ffb7ba1a04
Dan Wilkins <dan.wilkins@stanford.edu>
2018-05-02 16:27:07 -0700

    FluxHistogram class for plotting flux distributions from light curves

---
+++ flux_histogram.py
@@ -0,0 +1,45 @@
+"""
+pylag.flux_histogram
+
+Class to plot flux histograms from light curves
+"""
+from .lightcurve import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from scipy.stats import norm
+
+
+class FluxHistogram(object):
+    def __init__(self, lc, bins=50, log=False, plot=True):
+        if isinstance(lc, list):
+            lc = LightCurve().concatenate(lc)
+        lc = lc.remove_gaps()
+
+        r = lc.rate
+        if log:
+            r = np.log10(r)
+
+        self.mu, self.sigma = self.fit_gaussian(r)
+
+        if plot:
+            self._fig, self._ax = plt.subplots()
+            self.freq, self.bins, _ = self._ax.hist(r, bins=bins, normed=1)
+            if log:
+                self._ax.set_xlabel('log(Count Rate)')
+            else:
+                self._ax.set_xlabel('Count Rate')
+            self._ax.set_ylabel('Frequency')
+        else:
+            self.freq, self.bins = np.histogram(r, bins=bins, normed=1)
+
+        self.gaussian_fit = mlab.normpdf(self.bins, self.mu, self.sigma)
+        if plot:
+            self._ax.plot(self.bins, self.gaussian_fit, 'r--', linewidth=2)
+            self._fig.show()
+
+    @staticmethod
+    def fit_gaussian(r):
+        mu, sigma = norm.fit(r)
+        return mu, sigma
