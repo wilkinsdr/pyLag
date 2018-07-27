e87e0c3fbce68c9dc88e9efee53cbf5d679d0a30
Dan Wilkins <dan.wilkins@stanford.edu>
2018-07-27 10:51:54 -0700

    Binning bispectrum into frequencies (1D and 2D) across light curve segments

--- bispectrum.py
+++ bispectrum.py
@@ -2,7 +2,8 @@ from .lightcurve import *
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-
+from scipy.stats import binned_statistic_2d
+from .plotter import DataSeries
 
 class Bispectrum(object):
     def __init__(self, lc):
@@ -81,3 +82,138 @@ class Bispectrum(object):
         cb.set_label('Biphase / rad')
 
 
+class BinnedBispectrum(Bispectrum):
+    def __init__(self, lc, bins):
+        self.freq1, self.freq2 = np.meshgrid(bins.bin_cent, bins.bin_cent)
+        self.bispec = self.calculate_bispectrum(lc, bins)
+        self.bicoherence = self.calculate_bicoherence(lc, bins)
+        self.biphase = np.angle(self.bispec)
+
+    def calculate_bispectrum(self, lclist, bins):
+        freq1 = []
+        freq2 = []
+        bispec = []
+        for lc in lclist:
+            f1, f2 = self.bispec_freq(lc)
+            freq1.append(f1.flatten())
+            freq2.append(f2.flatten())
+            bispec.append(self.bispec_segment(lc).flatten())
+
+        freq1 = np.hstack(freq1)
+        freq2 = np.hstack(freq2)
+        bispec = np.hstack(bispec)
+        real = np.real(bispec)
+        imag = np.imag(bispec)
+
+        real_binned, _, _, _ = binned_statistic_2d(freq1, freq2, real, statistic='mean', bins=bins.bin_edges)
+        imag_binned, _, _, _ = binned_statistic_2d(freq1, freq2, imag, statistic='mean', bins=bins.bin_edges)
+        bispec_binned = real_binned + 1j * imag_binned
+
+        return bispec_binned
+
+    def calculate_bicoherence(self, lclist, bins):
+        freq1 = []
+        freq2 = []
+        norm1 = []
+        norm2 = []
+        bispec = []
+        for lc in lclist:
+            f1, f2 = self.bispec_freq(lc)
+            n1, n2 = self.bispec_segment_norm(lc)
+            freq1.append(f1.flatten())
+            freq2.append(f2.flatten())
+            norm1.append(n1.flatten())
+            norm2.append(n2.flatten())
+            bispec.append(self.bispec_segment(lc).flatten())
+
+        freq1 = np.hstack(freq1)
+        freq2 = np.hstack(freq2)
+        norm1 = np.hstack(norm1)
+        norm2 = np.hstack(norm2)
+        bispec = np.hstack(bispec)
+        real = np.real(bispec)
+        imag = np.imag(bispec)
+
+        real_binned, _, _, _ = binned_statistic_2d(freq1, freq2, real, statistic='sum', bins=bins.bin_edges)
+        imag_binned, _, _ , _= binned_statistic_2d(freq1, freq2, imag, statistic='sum', bins=bins.bin_edges)
+        bispec_binned = real_binned + 1j * imag_binned
+
+        norm1_binned, _, _, _ = binned_statistic_2d(freq1, freq2, norm1, statistic='sum', bins=bins.bin_edges)
+        norm2_binned, _, _, _ = binned_statistic_2d(freq1, freq2, norm2, statistic='sum', bins=bins.bin_edges)
+
+        bicoh_binned = np.abs(bispec_binned)**2 / (norm1_binned * norm2_binned)
+
+        return bicoh_binned
+
+
+class BinnedBispectrum1D(Bispectrum):
+    def __init__(self, lc, bins):
+        self.freq = bins.bin_cent
+        self.freq_err = bins.x_error()
+        self.bispec = self.calculate_bispectrum(lc, bins)
+        self.bicoherence = self.calculate_bicoherence(lc, bins)
+        self.biphase = np.angle(self.bispec)
+
+    def calculate_bispectrum(self, lclist, bins):
+        freq1 = []
+        freq2 = []
+        bispec = []
+        for lc in lclist:
+            f1, f2 = self.bispec_freq(lc)
+            freq1.append(f1.flatten())
+            freq2.append(f2.flatten())
+            bispec.append(self.bispec_segment(lc).flatten())
+
+        freq1 = np.hstack(freq1)
+        freq2 = np.hstack(freq2)
+        sumfreq = freq1 + freq2
+        bispec = np.hstack(bispec)
+        real = np.real(bispec)
+        imag = np.imag(bispec)
+
+        real_binned, _, _ = binned_statistic(sumfreq, real, statistic='mean', bins=bins.bin_edges)
+        imag_binned, _, _ = binned_statistic(sumfreq, imag, statistic='mean', bins=bins.bin_edges)
+        bispec_binned = real_binned + 1j * imag_binned
+
+        return bispec_binned
+
+    def calculate_bicoherence(self, lclist, bins):
+        freq1 = []
+        freq2 = []
+        norm1 = []
+        norm2 = []
+        bispec = []
+        for lc in lclist:
+            f1, f2 = self.bispec_freq(lc)
+            n1, n2 = self.bispec_segment_norm(lc)
+            freq1.append(f1.flatten())
+            freq2.append(f2.flatten())
+            norm1.append(n1.flatten())
+            norm2.append(n2.flatten())
+            bispec.append(self.bispec_segment(lc).flatten())
+
+        freq1 = np.hstack(freq1)
+        freq2 = np.hstack(freq2)
+        sumfreq = freq1 + freq2
+        norm1 = np.hstack(norm1)
+        norm2 = np.hstack(norm2)
+        bispec = np.hstack(bispec)
+        real = np.real(bispec)
+        imag = np.imag(bispec)
+
+        real_binned, _, _ = binned_statistic(sumfreq[np.logical_and(freq1 > 0, freq2 > 0)], real[np.logical_and(freq1 > 0, freq2 > 0)], statistic='mean', bins=bins.bin_edges)
+        imag_binned, _, _ = binned_statistic(sumfreq[np.logical_and(freq1 > 0, freq2 > 0)], imag[np.logical_and(freq1 > 0, freq2 > 0)], statistic='mean', bins=bins.bin_edges)
+        bispec_binned = real_binned + 1j * imag_binned
+
+        norm1_binned, _, _ = binned_statistic(sumfreq[np.logical_and(freq1 > 0, freq2 > 0)], norm1[np.logical_and(freq1 > 0, freq2 > 0)], statistic='mean', bins=bins.bin_edges)
+        norm2_binned, _, _ = binned_statistic(sumfreq[np.logical_and(freq1 > 0, freq2 > 0)], norm2[np.logical_and(freq1 > 0, freq2 > 0)], statistic='mean', bins=bins.bin_edges)
+
+        bicoh_binned = np.abs(bispec_binned)**2 / (norm1_binned * norm2_binned)
+
+        return bicoh_binned
+
+    def bicoherence_series(self):
+        return DataSeries(x=(self.freq, self.freq_err), y=self.bicoherence, xlabel='f1 + f2 / Hz', xscale='log', ylabel='Bicoherence', yscale='linear')
+
+    def biphase_series(self):
+        return DataSeries(x=(self.freq, self.freq_err), y=self.biphase, xlabel='f1 + f2 / Hz', xscale='log', ylabel='Biphase / rad', yscale='linear')
