be222931bfeb5bcc6c8ba26d119c0832fa376f92
Dan Wilkins <dan.wilkins@stanford.edu>
2018-12-06 12:20:47 -0800

    steppar method in Fit class to step through parameter values and calculate best fit for each to get confidence contours (and steppar2 for stepping 2 parameters)
    Actual fit is now in _dofit method - this returns the result ot be interpreted by perform_fit, steppar or steppar2

--- fit.py
+++ fit.py
@@ -93,7 +93,7 @@ class Fit(object):
                 self.xerror = self.xerror[np.logical_not(np.isnan(self.yerror))]
             self.yerror = self.yerror[np.logical_not(np.isnan(self.yerror))]
 
-    def perform_fit(self, fit_range=None):
+    def _dofit(self, params, fit_range=None):
         if isinstance(fit_range, tuple):
             xmin, xmax = fit_range
             xd = self.xdata[np.logical_and(self.xdata >= xmin, self.xdata < xmax)]
@@ -105,9 +105,13 @@ class Fit(object):
             ye = self.yerror
         else:
             raise ValueError("pylag Fit perform_fit ERROR: Unexpected value for fit_range")
-        self.minimizer = lmfit.Minimizer(self.statistic, self.params, fcn_args=(xd, yd, ye, self.modelfn), xtol=1e-10, ftol=1e-10)
-        self.fit_result = self.minimizer.minimize()
+        self.minimizer = lmfit.Minimizer(self.statistic, params, fcn_args=(xd, yd, ye, self.modelfn), xtol=1e-10, ftol=1e-10)
+        result = self.minimizer.minimize()
+        return result
         #self.fit_result = lmfit.minimize(self.statistic, self.params, args=(xd, yd, ye, self.modelfn))
+
+    def perform_fit(self, fit_range=None):
+        self.fit_result = self._dofit(self.params, fit_range)
 
     def confidence_intervals(self, default_stderr=0.1):
         for parname in self.fit_result.params:
@@ -117,43 +121,102 @@ class Fit(object):
         self.ci_result = lmfit.conf_interval(self.minimizer, self.fit_result)
         self.report_ci()
 
+    def steppar(self, step_param, start, stop, steps, log_space=False, fit_range=None):
+        import copy
+
+        if log_space:
+            ratio = np.exp(np.log(stop / start) / steps)
+            par_steps = start * ratio ** np.array(range(steps))
+        else:
+            par_steps = np.linspace(start, stop, steps)
+
+        step_params = copy.copy(self.params)
+        step_params[step_param].vary = False
+
+        stat = []
+        fit_params = []
+
+        for value in par_steps:
+            step_params[step_param].value = value
+            result = self._dofit(step_params, fit_range)
+            stat.append(result.chisqr)
+            fit_params.append(result.params)
+
+        return par_steps, stat, fit_params
+
+    def steppar2(self, step_param1, start1, stop1, steps1, step_param2, start2, stop2, steps2, log_space1=False, log_space2=False, fit_range=None):
+        import copy
+
+        if log_space1:
+            ratio1 = np.exp(np.log(stop1 / start1) / steps1)
+            par_steps1 = start1 * ratio1 ** np.array(range(steps1))
+        else:
+            par_steps1 = np.linspace(start1, stop1, steps1)
+        if log_space2:
+            ratio2 = np.exp(np.log(stop2 / start2) / steps2)
+            par_steps2 = start2 * ratio2 ** np.array(range(steps2))
+        else:
+            par_steps2 = np.linspace(start2, stop2, steps2)
+
+        step_params = copy.copy(self.params)
+        step_params[step_param1].vary = False
+        step_params[step_param2].vary = False
+
+        stat = np.zeros((len(par_steps1), len(par_steps2)))
+        fit_params = []
+
+        for i1, value1 in enumerate(par_steps1):
+            fit_params.append([])
+            step_params[step_param1].value = value1
+            for i2, value2 in enumerate(par_steps2):
+                step_params[step_param2].value = value2
+                result = self._dofit(step_params, fit_range)
+                stat[i1,i2] = result.chisqr
+                fit_params[-1].append(result.params)
+
+        return par_steps1, par_steps2, stat, fit_params
+
     def report(self):
         lmfit.report_fit(self.fit_result)
 
     def report_ci(self):
         lmfit.printfuncs.report_ci(self.ci_result)
 
-    def fit_function(self, x=None):
+    def fit_function(self, x=None, params=None):
         if x is None:
             x = self.xdata
-        return x, self.modelfn(self.fit_result.params, x)
+        if params is None:
+            params = self.fit_result.params
+        return x, self.modelfn(params, x)
 
-    def ratio(self):
-        r = self.ydata / self.modelfn(self.fit_result.params, self.xdata)
+    def ratio(self, params=None):
+        if params is None:
+            params = self.fit_result.params
+        r = self.ydata / self.modelfn(params, self.xdata)
         if self.yerror is not None:
             e = r * (self.yerror / self.ydata)
             return r, e
         else:
             return r
 
-    def _getdataseries(self, x=None):
-        x, y = self.fit_function(x)
+    def _getdataseries(self, x=None, params=None):
+        x, y = self.fit_function(x, params)
         return DataSeries(x, y)
 
-    def _getratioseries(self, x=None):
+    def _getratioseries(self, x=None, params=None):
         if self.xerror is not None:
             x = (self.xdata, self.xerror)
         else:
             x = self.xdata
         xlabel, xscale, ylabel, yscale = self.data_obj._getplotaxes()
-        return DataSeries(x, self.ratio(), xlabel=xlabel, xscale=xscale, ylabel='Data / Model', yscale='linear')
+        return DataSeries(x, self.ratio(params), xlabel=xlabel, xscale=xscale, ylabel='Data / Model', yscale='linear')
 
-    def plot_fit(self, x=None):
-        p = Plot([self.data_obj, self._getdataseries(x)])
+    def plot_fit(self, x=None, params=None):
+        p = Plot([self.data_obj, self._getdataseries(x, params)])
         p.marker_series = ['+', '-']
         return p
 
-    def plot_ratio(self, x=None):
-        p = Plot(self._getratioseries(x))
+    def plot_ratio(self, x=None, params=None):
+        p = Plot(self._getratioseries(x, params))
         p.marker_series = ['+', '-']
         return p
