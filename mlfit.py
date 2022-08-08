0515111f8495b4dacc61f9df4c315980b82f549b
Dan Wilkins <dan.wilkins@stanford.edu>
2022-08-07 19:11:40 -0700

    Finish implementation of MLCrossSpectrum. Still needs debugging though!

--- mlfit.py
+++ mlfit.py
@@ -17,17 +17,27 @@ import copy
 
 from .lightcurve import *
 from .binning import *
-from .fit import *
-from .plotter import *
 from .model import *
+from .plotter import *
 
 class MLFit(object):
-    def __init__(self, t, y, psdnorm=1., Nf=10, fbins=None, model=None, comp_name='', model_args={}):
+    def __init__(self, t, y, yerr=None, psdnorm=1., Nf=10, fbins=None, model=None, component_name=None, model_args={}, noise='errors'):
         self.time = np.array(t)
         self.dt = np.min(np.diff(self.time))
 
         # the linear algebra we'll be doing requires the data points as a column vector (in matrix form)
         self.data = y[:, np.newaxis]
+
+        self.yerr = yerr
+
+        if noise == 'errors' and yerr is not None:
+            self.noise = self.yerr**2
+        elif noise == 'mean':
+            self.noise = np.ones_like(self.time) * np.mean(self.yerr)**2
+        elif isinstance(noise, (float, int)):
+            self.noise = noise * np.ones_like(self.time)
+        else:
+            self.noise = None
 
         self.psdnorm = psdnorm
 
@@ -49,23 +59,32 @@ class MLFit(object):
         # so we don't have to do this for every change in parameter values
         # note the factor of 2 to integrate over the negative frequencies too!
         self.cos_integral = np.array([np.array([(1. / (np.pi * t)) * (np.sin(2*np.pi*fmax*t) - np.sin(2*np.pi*fmin*t))
-                                                if t > 0 else (fmax - fmin)
+                                                if t > 0 else 2. * (fmax - fmin)
                                                 for t in self.tau.flatten()]).reshape(self.tau.shape)
                                       for fmin, fmax in zip(self.fbins.bin_start, self.fbins.bin_end)])
+
+        # self.cos_integral = np.array([np.array([(1. / (2. * np.pi * t)) * (np.sin(2*np.pi*fmax*t) - np.sin(2*np.pi*fmin*t))
+        #                                         if t > 0 else (fmax - fmin)
+        #                                         for t in self.tau.flatten()]).reshape(self.tau.shape)
+        #                               for fmin, fmax in zip(self.fbins.bin_start, self.fbins.bin_end)])
 
         if model is None:
             self.model = None
         elif isinstance(model, Model):
             self.model = model
         else:
-            self.model = model(component_name=comp_name, **model_args)
+            self.model = model(component_name=component_name, **model_args)
 
-        self.comp_name = comp_name
+        self.comp_name = component_name
+        self.prefix = component_name + "_" if component_name is not None else ''
         self.params = self.get_params()
 
         self.fit_result = None
 
-    def log_likelihood(self, params, eval_gradient=True, delta=1e-3):
+    def log_likelihood(self, params, eval_gradient=True):
+
+        #print(param2array(params))
+
         c = self.cov_matrix(params)
 
         try:
@@ -73,11 +92,17 @@ class MLFit(object):
         except np.linalg.LinAlgError:
             # try:
             # if the matrix is sibgular, perturb the diagonal and try again
-            L = cholesky(c + 1e-3 * np.eye(c.shape[0]), lower=True, check_finite=False)
+            try:
+                L = cholesky(c + 1e-3 * np.eye(c.shape[0]), lower=True, check_finite=False)
+            except np.linalg.LinAlgError:
+                par_str = ", ".join(["%s: %g" % (p, params[p].value) for p in params])
+                print("WARNING: Couldn't invert covariance matrix with parameters " + par_str)
+                return (1e6, np.zeros(len(params)) - 1e6) if eval_gradient else -np.inf
+                # raise np.linalg.LinAlgError("Couldn't invert covariance matrix with parameters " + par_str)
             # except np.linalg.LinAlgError:
             #     return (-np.inf, np.zeros(len(params))) if eval_gradient else -np.inf
         except ValueError:
-            return (-np.inf, np.zeros(len(params))) if eval_gradient else -np.inf
+            return (np.inf, np.zeros(len(params))) if eval_gradient else -np.inf
 
         alpha = cho_solve((L, True), self.data, check_finite=False)
 
@@ -87,7 +112,7 @@ class MLFit(object):
         log_likelihood = log_likelihood_dims.sum(-1)
 
         if eval_gradient:
-            c_gradient = self.cov_matrix_deriv(params, delta)
+            c_gradient = self.cov_matrix_deriv(params)
             tmp = np.einsum("ik,jk->ijk", alpha, alpha)
             tmp -= cho_solve((L, True), np.eye(c.shape[0]))[:, :, np.newaxis]
             gradient_dims = 0.5 * np.einsum("ijl,ijk->kl", tmp, c_gradient)
@@ -140,10 +165,12 @@ class MLPSD(MLFit):
         # to fit a Gaussian process, we should make sure our data has zero mean
         y -= np.mean(y)
 
+        yerr = lc.error
+
         # RMS normalisation for PSD
         psdnorm = 2 * np.min(np.diff(lc.time)) / (lc.mean() ** 2 * lc.length)
 
-        MLFit.__init__(self, t, y, psdnorm=psdnorm, **kwargs)
+        MLFit.__init__(self, t, y, yerr, psdnorm=psdnorm, **kwargs)
 
         self.psd = self.get_psd()
         self.psd_error = None
@@ -157,9 +184,15 @@ class MLPSD(MLFit):
             psd = self.model(params, self.fbins.bin_cent) / self.psdnorm
 
         cov = np.sum(np.array([p * c for p, c in zip(psd, self.cos_integral)]), axis=0)
+
+        # add white noise along the leading diagonal
+        # this should be the Poisson noise term when calculating a PSD
+        if self.noise is not None:
+            cov += np.diag(self.noise)
+
         return cov
 
-    def cov_matrix_deriv(self, params, delta):
+    def cov_matrix_deriv(self, params, delta=1e-6):
         if self.model is None:
             psd = np.exp(np.array([params[p].value for p in params])) / self.psdnorm
 
@@ -210,40 +243,44 @@ class MLCrossSpectrum(MLFit):
         # to fit a cross spectrum, we stack the data vectors
         y = np.concatenate([y1, y2])
 
-        cpsdnorm = 2 * np.min(np.diff(lc1.time)) / (np.mean(np.concatenate([lc1.rate, lc2.rate])) ** 2 * lc1.length)
+        #cpsdnorm = 2 * np.min(np.diff(lc1.time)) / (np.mean(np.concatenate([lc1.rate, lc2.rate])) ** 2 * lc1.length)
+
+        cpsdnorm = 2 * np.min(np.diff(lc1.time)) / (lc1.mean() * lc2.mean() * lc1.length)
+
+        self.ac1 = None
+        self.ac2 = None
 
         if mlpsd1 is not None:
             self.mlpsd1 = mlpsd1
+            self.ac1 = self.mlpsd1.cov_matrix(self.mlpsd1.params)
         else:
-            self.mlpsd1 = MLPSD(lc1, model=psd_model, comp_name='psd1', **kwargs)
+            self.mlpsd1 = MLPSD(lc1, model=psd_model, component_name='psd1', **kwargs)
 
         if mlpsd2 is not None:
             self.mlpsd2 = mlpsd2
+            self.ac2 = self.mlpsd2.cov_matrix(self.mlpsd2.params)
         else:
-            self.mlpsd2 = MLPSD(lc2, model=psd_model, comp_name='psd2', **kwargs)
+            self.mlpsd2 = MLPSD(lc2, model=psd_model, component_name='psd2', **kwargs)
 
         self.freeze_psd = freeze_psd
-
-        self.autocov1 = None
-        self.autocov2 = None
 
         if cpsd_model is None:
             self.cpsd_model = None
         elif isinstance(cpsd_model, Model):
             self.cpsd_model = cpsd_model
         else:
-            self.cpsd_model = cpsd_model(component_name='cpsd_', **cpsd_model_args)
+            self.cpsd_model = cpsd_model(component_name='cpsd', **cpsd_model_args)
 
         if lag_model is None:
             self.lag_model = None
         elif isinstance(lag_model, Model):
             self.lag_model = lag_model
         else:
-            self.lag_model = lag_model(component_name='lag_', **lag_model_args)
+            self.lag_model = lag_model(component_name='lag', **lag_model_args)
 
         MLFit.__init__(self, t, y, psdnorm=cpsdnorm, **kwargs)
 
-        # we probably don't need the sine integrals since they go to zero when we integrate over -ve frequencies
+        #we probably don't need the sine integrals since they go to zero when we integrate over -ve frequencies
         # self.sin_integral = np.array(
         #     [np.array([(1. / (2. * np.pi * t)) * (np.cos(2 * np.pi * fmin * t) - np.cos(2 * np.pi * fmax * t))
         #                if t > 0 else 0.
@@ -257,43 +294,150 @@ class MLCrossSpectrum(MLFit):
             params += self.mlpsd2.get_params()
         if self.cpsd_model is None:
             for i in range(len(self.fbins)):
-                params.add('%sln_cpsd%01d' % (self.comp_name, i), 1., vary=True, min=-30., max=30.)
+                params.add('%sln_cpsd%01d' % (self.prefix, i), 1., vary=True, min=-30., max=30.)
         else:
             params += self.cpsd_model.get_params()
         if self.lag_model is None:
             for i in range(len(self.fbins)):
-                params.add('%slag%01d' % (self.comp_name, i), 1., vary=True, min=-np.pi, max=np.pi)
+                params.add('%slag%01d' % (self.prefix, i), 0., vary=True, min=-np.pi, max=np.pi)
         else:
             params += self.lag_model.get_params()
 
         return params
 
+    def fit_psd(self):
+        print("Fitting PSD of light curve 1...")
+        self.mlpsd1.fit()
+        self.ac1 = self.mlpsd1.cov_matrix(self.mlpsd1.params)
+
+        print("Fitting PSD of light curve 2...")
+        self.mlpsd2.fit()
+        self.ac2 = self.mlpsd1.cov_matrix(self.mlpsd2.params)
+
+        # set an initial estimate of the cross power spectrum as the average of the two band powers
+        # this helps avoid uninvertable matrices in the first step of the fit!
+        for i in range(len(self.fbins)):
+            self.params['ln_cpsd%01d' % i].value = 0.5 * (self.mlpsd1.psd[i] + self.mlpsd2.psd[i])
+
     def cross_cov_matrix(self, params):
         # if no model is specified, the PSD model is just the PSD value in each frequency bin
         if self.cpsd_model is None:
-            cpsd = np.exp(np.array([params['%sln_cpsd%01d' % (self.comp_name, i)].value for i in range(len(self.fbins))])) / self.psdnorm
+            cpsd = np.exp(np.array([params['%sln_cpsd%01d' % (self.prefix, i)].value for i in range(len(self.fbins))])) / self.psdnorm
         else:
             cpsd = self.cpsd_model(params, self.fbins.bin_cent) / self.psdnorm
 
         # likewise for the (phase) lags
         if self.lag_model is None:
-            lags = np.array([params['%slag%01d' % (self.comp_name, i)].value for i in range(len(self.fbins))])
+            lags = np.array([params['%slag%01d' % (self.prefix, i)].value for i in range(len(self.fbins))])
         else:
             lags = self.lag_model(params, self.fbins.bin_cent)
 
         # TODO: is this correct or do we need the sine terms? (I think we don't if we're integrating over the -ve freq terms)
-        #cov = np.sum(np.array([p * (c * np.cos(phi) + s * np.sin(phi)) for p, c, s, phi in zip(cpsd, self.cos_integral, self.sin_integral, lags)]), axis=0)
+        #cov = np.sum(np.array([p * (c * np.cos(phi) - s * np.sin(phi)) for p, c, s, phi in zip(cpsd, self.cos_integral, self.sin_integral, lags)]), axis=0)
         cov = np.sum(np.array([p * c * np.cos(phi) for p, c, phi in zip(cpsd, self.cos_integral, lags)]), axis=0)
         return cov
 
     def cov_matrix(self, params):
         if self.freeze_psd:
-            ac1 = self.autocov1
-            ac2 = self.autocov2
+            if self.ac1 is None or self.ac2 is None:
+                raise AssertionError("Autocovariance matrices are not available. Did you fit the PSDs?")
+            ac1 = self.ac1
+            ac2 = self.ac2
         else:
             ac1 = self.mlpsd1.cov_matrix(params)
-            ac2 - self.mlpsd2.cov_matrix(params)
+            ac2 = self.mlpsd2.cov_matrix(params)
 
         cc = self.cross_cov_matrix(params)
 
         return np.vstack([np.hstack([ac1, cc.T]), np.hstack([cc, ac2])])
+
+    def cross_cov_matrix_deriv(self, params, delta=1e-6):
+        if self.cpsd_model is None:
+            # if no model is specified, the PSD model is just the PSD value in each frequency bin
+            if self.cpsd_model is None:
+                cpsd = np.exp(np.array(
+                    [params['%sln_cpsd%01d' % (self.prefix, i)].value for i in range(len(self.fbins))])) / self.psdnorm
+            else:
+                cpsd = self.cpsd_model(params, self.fbins.bin_cent) / self.psdnorm
+
+            # likewise for the (phase) lags
+            if self.lag_model is None:
+                lags = np.array([params['%slag%01d' % (self.prefix, i)].value for i in range(len(self.fbins))])
+            else:
+                lags = self.lag_model(params, self.fbins.bin_cent)
+
+            if self.cpsd_model is None:
+                cpsd_derivs = [c * np.cos(phi) * p for p, c, phi in zip(cpsd, self.cos_integral, lags)]
+                #cpsd_derivs = [(c * np.cos(phi) + s * np.sin(phi)) * p for p, c, s, phi in zip(cpsd, self.cos_integral, self.sin_integral, lags)]
+            else:
+                # TODO: implement chain rule derivatives for functions
+                return NotImplemented
+
+            if self.lag_model is None:
+                lag_derivs = [-1 * p * c * np.sin(phi) for p, c, phi in zip(cpsd, self.cos_integral, lags)]
+                #lag_derivs = [-1 * p * (c * np.sin(phi) + s * np.cos(phi)) for p, c, s, phi in zip(cpsd, self.cos_integral, self.sin_integral, lags)]
+            else:
+                # TODO: implement chain rule derivatives for functions
+                return NotImplemented
+
+            # this is the stack of (1) the derivatives w.r.t. the cross powers (multiplied by p when we're using the log)
+            # and (2) the phases
+            return np.stack(cpsd_derivs + lag_derivs, axis=-1)
+
+    def cov_matrix_deriv(self, params):
+        cc = self.cross_cov_matrix_deriv(params)
+
+        if self.freeze_psd:
+            Z = np.zeros_like(self.ac1)
+            return np.stack(
+                [np.vstack([np.hstack([Z, cc[..., p].T]), np.hstack([cc[..., p], Z])]) for p in
+                 range(len(self.params))], axis=-1)
+
+        else:
+            ac1 = self.mlpsd1.cov_matrix_deriv(params)
+            ac2 = self.mlpsd2.cov_matrix_deriv(params)
+            return np.stack(
+                [np.vstack([np.hstack([ac1[..., p], cc[..., p].T]), np.hstack([cc[..., p], ac2[..., p]])]) for p in
+                 range(len(self.params))], axis=-1)
+
+
+
+        return np.stack([np.vstack([np.hstack([ac1[...,p], cc[...,p].T]), np.hstack([cc[...,p], ac2[...,p]])]) for p in range(len(self.params))], axis=-1)
+
+    def get_cpsd(self, params=None):
+        if params is None:
+            params = self.params
+
+        if self.cpsd_model is None:
+            return np.array([self.params['%sln_cpsd%01d' % (self.prefix, i)].value for i in range(len(self.fbins))])
+        else:
+            return np.log(self.cpsd_model(self.params, self.fbins.bin_cent))
+
+    def get_lag(self, params=None, time_lag=True):
+        if params is None:
+            params = self.params
+
+        if self.lag_model is None:
+            lag = np.array([self.params['%slag%01d' % (self.prefix, i)].value for i in range(len(self.fbins))])
+        else:
+            lag = np.log(self.cpsd_model(self.params, self.fbins.bin_cent))
+
+        return lag / (2. * np.pi * self.fbins.bin_cent) if time_lag else lag
+
+    def process_fit_results(self, fit_result, params):
+        self.cpsd = self.get_cpsd()
+        if self.cpsd_model is None:
+            self.cpsd_error = fit_result.hess_inv(fit_result.x)[:len(self.fbins)] ** 0.5
+        else:
+            return NotImplemented
+            # # calculate the error on each PSD point from the error on each parameter
+            # psd_deriv = self.model.eval_gradient(params, self.fbins.bin_cent)
+            # self.psd_error = np.sum([e * psd_deriv[..., i] for i, e in enumerate(self.param_error)], axis=0) / self.psd
+            # if np.any(np.isnan(self.psd_error)):
+            #     self.psd_error = None
+
+        self.lag = self.get_lag()
+        if self.cpsd_model is None:
+            self.lag_error = fit_result.hess_inv(fit_result.x)[len(self.fbins):] ** 0.5 / (2. * np.pi * self.fbins.bin_cent)
+        else:
+            return NotImplemented
